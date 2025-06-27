05454c2729e4c96355aa9a1b24ef434f2548d711
Zaharov Maksim <zaharovmax2020@gmail.com>
2025-06-27 15:58:29 +0700

    v 2

--- logic.py
+++ logic.py
@@ -1,5 +1,8 @@
 from random import randint
 import requests
+import threading
+import time
+from datetime import datetime
 
 class Pokemon:
     pokemons = {}
@@ -7,10 +10,11 @@ class Pokemon:
     def __init__(self, pokemon_trainer):
 
         self.pokemon_trainer = pokemon_trainer   
-
+        self.age = 0 
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.birth_date = datetime.now()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -20,7 +24,7 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return (data['forms'][0]['name'])
+            return data['sprites']['front_default']
         else:
             return "Pikachu"
     
@@ -30,18 +34,30 @@ class Pokemon:
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
-            return data['sprites']['front_default']
+            return (data['forms'][0]['name'])
         else:
             return "Pikachu"
 
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покемона: {self.name}\nВозраст: {self.age} лет"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+    
+    # Метода класса для увеличивает возраст
+    def add_year(self):
+        self.age += 1
+        print(f"{self.name} стал старше! Теперь ему {self.age} лет.")
 
+# Функция для увеличения возраста всех покемонов
+def age_updater():
+    while True:
+        time.sleep(60)  
+        for trainer, pokemon in Pokemon.pokemons.items():
+            pokemon.add_year()
 
-
+age_thread = threading.Thread(target=age_updater, daemon=True)
+age_thread.start()
--- main.py
+++ main.py
@@ -14,6 +14,13 @@ def go(message):
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
+@bot.message_handler(commands=['age'])
+def get_age(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pokemon = Pokemon.pokemons[message.from_user.username]
+        bot.reply_to(message, f"Твоему покемону {pokemon.name} сейчас {pokemon.age} лет")
+    else:
+        bot.reply_to(message, "У тебя еще нет покемона. Напиши /go чтобы создать его")
 
 bot.infinity_polling(none_stop=True)
 
